b8ac84cce328c57f65d769009dc7dac0028019c3
EvilAnkityadav <79680901+EvilAnkityadav@users.noreply.github.com>
2021-04-21 22:15:28 +0530

    what_do_i_eat.c

---
+++ what_do_i_eat.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int n; 
+scanf("%d",&n);
+switch (n)
+{
+    case 1: printf("Food item - Pizza \nPrice - 239/-");
+    break;
+    case 2: printf("Food item - Burgur \nPrice - 129/-");
+    break;
+    case 3: printf("Food item - Pasta \nPrice - 179/-");
+    break;
+    case 4: printf("Food item - French fries \nPrice - 99/-");
+    break;
+    case 5: printf("Food item - Sandwitch \nPrice -149/-");
+    break;
+}
+return 0;
+}
