068740d46d0f3c5261050d324834facda966c950
EvilAnkityadav <79680901+EvilAnkityadav@users.noreply.github.com>
2021-04-27 21:04:30 +0530

    loops.c
    Write a C program to print the following pattern 01 0101 010101 01010101 0101010101 010101010101

---
+++ loops.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+int main(){
+for (int  i = 0; i < 6; i++)
+{
+    for (int j = 0; j < i+1; j++)
+    {
+    printf("01");
+    }
+    printf(" ");
+}
+return 0;
+}
